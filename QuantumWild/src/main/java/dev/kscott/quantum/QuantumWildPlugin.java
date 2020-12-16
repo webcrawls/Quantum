9ca656c796db255f9c9650fd75ba5d4364b5a89a
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-16 16:37:28 -0500

    remove code carried over from Quantum

--- QuantumWild/src/main/java/dev/kscott/quantum/QuantumWildPlugin.java
+++ QuantumWild/src/main/java/dev/kscott/quantum/QuantumWildPlugin.java
@@ -1,33 +1,13 @@
 package dev.kscott.quantum;
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 import com.google.inject.Singleton;
-import dev.kscott.quantum.command.QuantumCommand;
-import dev.kscott.quantum.inject.CommandModule;
-import dev.kscott.quantum.inject.LocationModule;
-import dev.kscott.quantum.inject.PluginModule;
-import dev.kscott.quantum.location.LocationProvider;
-import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.checkerframework.checker.nullness.qual.NonNull;
 
 @Singleton
 public final class QuantumWildPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        final @NonNull Injector injector = Guice.createInjector(
-                new PluginModule(this),
-                new CommandModule(this),
-                new LocationModule(this)
-        );
-
-        injector.getInstance(QuantumCommand.class);
-
-        final @NonNull LocationProvider locationProvider = injector.getInstance(LocationProvider.class);
-
     }
 
     @Override
