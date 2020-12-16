6589ebf53a4c5b2b61d0aaeb53b1c2410131e494
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-16 17:19:18 -0500

    finish RulesetRegistry

--- Quantum/src/main/java/dev/kscott/quantum/rule/ruleset/RulesetRegistry.java
+++ Quantum/src/main/java/dev/kscott/quantum/rule/ruleset/RulesetRegistry.java
@@ -2,10 +2,40 @@ package dev.kscott.quantum.rule.ruleset;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * The main class that controls access and stores rulesets.
+ * If a ruleset isn't registered here, it doesn't to Quantum.
+ */
 public class RulesetRegistry {
 
-    public void register(final @NonNull QuantumRuleset ruleset) {
+    /**
+     * The map where all rulesets are stored
+     */
+    private final @NonNull Map<String, QuantumRuleset> rulesetMap;
 
+    /**
+     * Constructs the RulesetRegistry and it's internal map
+     */
+    public RulesetRegistry() {
+        this.rulesetMap = new HashMap<>();
+    }
+
+    /**
+     * Registers a Ruleset under it's id
+     * @param ruleset Ruleset to register
+     * @throws IllegalStateException if a ruleset with the same id is already registered
+     */
+    public void register(final @NonNull QuantumRuleset ruleset) throws IllegalStateException {
+        final @NonNull String id = ruleset.getId();
+
+        if (rulesetMap.containsKey(id)) {
+            throw new IllegalStateException("Ruleset with the id '"+id+"' is already registered!");
+        }
+
+        this.rulesetMap.put(id, ruleset);
     }
 
 }
