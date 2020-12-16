62c10056f980141c9ab01c813a19ac5c7b5804e8
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-16 16:43:55 -0500

    remove more old code

--- QuantumSpawn/src/main/java/dev/kscott/quantumspawn/QuantumSpawnPlugin.java
+++ QuantumSpawn/src/main/java/dev/kscott/quantumspawn/QuantumSpawnPlugin.java
@@ -1,32 +1,11 @@
 package dev.kscott.quantumspawn;
-
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import com.google.inject.Singleton;
-import dev.kscott.quantumwild.command.QuantumCommand;
-import dev.kscott.quantumwild.inject.CommandModule;
-import dev.kscott.quantumwild.inject.LocationModule;
-import dev.kscott.quantumwild.inject.PluginModule;
-import dev.kscott.quantumwild.location.LocationProvider;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.checkerframework.checker.nullness.qual.NonNull;
 
-@Singleton
 public final class QuantumSpawnPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        final @NonNull Injector injector = Guice.createInjector(
-                new PluginModule(this),
-                new CommandModule(this),
-                new LocationModule(this)
-        );
-
-        injector.getInstance(QuantumCommand.class);
-
-        final @NonNull LocationProvider locationProvider = injector.getInstance(LocationProvider.class);
-
     }
 
     @Override
