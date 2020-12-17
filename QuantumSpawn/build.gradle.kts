a80e5d496ed8e2cb678b1c1108257bef83defd24
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-17 01:34:22 -0500

    Add QS & QW build scripts

---
+++ QuantumSpawn/build.gradle.kts
@@ -0,0 +1,9 @@
+dependencies {
+    implementation("org.spongepowered:configurate-hocon:4.0.0")
+
+    implementation("net.kyori:adventure-api:4.3.0")
+    implementation("net.kyori:adventure-platform-bukkit:4.0.0-SNAPSHOT")
+    implementation("net.kyori:adventure-text-minimessage:4.1.0-SNAPSHOT")
+
+    api("cloud.commandframework:cloud-paper:1.2.0")
+}
