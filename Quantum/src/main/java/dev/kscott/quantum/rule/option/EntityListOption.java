5e8c426bdf468891a7f159943f659e21186abffb
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-17 17:48:32 -0500

    [Quantum] add some more rule options

---
+++ Quantum/src/main/java/dev/kscott/quantum/rule/option/EntityListOption.java
@@ -0,0 +1,13 @@
+package dev.kscott.quantum.rule.option;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public class EntityListOption extends QuantumRuleOption<String[]> {
+
+    /**
+     * Constructs EntityListOption
+     */
+    public EntityListOption() {
+        super("entity-list");
+    }
+}
---
+++ Quantum/src/main/java/dev/kscott/quantum/rule/option/RadiusOption.java
@@ -0,0 +1,11 @@
+package dev.kscott.quantum.rule.option;
+
+public class RadiusOption extends QuantumRuleOption<Integer> {
+
+    /**
+     * Constructs BlockListOption
+     */
+    public RadiusOption() {
+        super("radius");
+    }
+}
