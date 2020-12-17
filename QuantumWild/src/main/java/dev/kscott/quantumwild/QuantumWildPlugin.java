606c4ff1d72da34c6097dbaff9ce7d4a58bde6e0
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-17 02:11:34 -0500

    [QuantumWild] inject modules

--- QuantumWild/src/main/java/dev/kscott/quantumwild/QuantumWildPlugin.java
+++ QuantumWild/src/main/java/dev/kscott/quantumwild/QuantumWildPlugin.java
@@ -2,18 +2,20 @@ package dev.kscott.quantumwild;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import dev.kscott.quantum.api.QuantumAPI;
+import dev.kscott.quantumwild.module.CommandModule;
+import dev.kscott.quantumwild.module.PluginModule;
 import dev.kscott.quantumwild.module.QuantumModule;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
-import org.checkerframework.checker.nullness.qual.Nullable;
 
 public final class QuantumWildPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
         final @NonNull Injector injector = Guice.createInjector(
-                new QuantumModule(this)
+                new QuantumModule(this),
+                new PluginModule(this),
+                new CommandModule(this)
         );
     }
 
