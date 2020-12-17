0bbfd5b194f145205ffa735ac63052327e6c4507
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-17 18:17:22 -0500

    [Quantum] fix EntityListOption id

--- Quantum/src/main/java/dev/kscott/quantum/rule/option/EntityListOption.java
+++ Quantum/src/main/java/dev/kscott/quantum/rule/option/EntityListOption.java
@@ -1,13 +1,11 @@
 package dev.kscott.quantum.rule.option;
 
-import org.checkerframework.checker.nullness.qual.NonNull;
-
 public class EntityListOption extends QuantumRuleOption<String[]> {
 
     /**
      * Constructs EntityListOption
      */
     public EntityListOption() {
-        super("entity-list");
+        super("entity-types");
     }
 }
