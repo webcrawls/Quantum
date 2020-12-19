ccaf1ec5d1cade033d1af12dade0bb9ecb83b2e6
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-18 22:55:42 -0500

    [QuantumWild] populate getCooldownToApply

--- QuantumWild/src/main/java/dev/kscott/quantumwild/wild/WildManager.java
+++ QuantumWild/src/main/java/dev/kscott/quantumwild/wild/WildManager.java
@@ -3,7 +3,7 @@ package dev.kscott.quantumwild.wild;
 import dev.kscott.quantumwild.config.Config;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.cacheddata.CachedMetaData;
-import net.milkbowl.vault.permission.Permission;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
@@ -30,17 +30,42 @@ public class WildManager {
         this.config = config;
     }
 
-    public long getCooldownToApply(final @NonNull Player player) {
+    /**
+     * Returns the amount of cooldown that this player will receive.
+     * This takes the player's world into consideration, and the value
+     * of {@link Config#isPerWorldCooldownEnabled()}
+     *
+     * @param player Player to get cooldown for
+     * @return amount of cooldown, in seconds
+     */
+    public int getCooldownToApply(final @NonNull Player player) {
         if (this.luckPerms == null) {
             return this.config.getFallbackCooldown();
         }
-
 
         final @NonNull CachedMetaData meta = this.luckPerms.getUserManager().getUser(player.getUniqueId())
                 .getCachedData()
                 .getMetaData();
 
-        return 0;
+        final @NonNull World world = player.getWorld();
+
+        if (this.config.isPerWorldCooldownEnabled()) {
+            final @Nullable String cooldownValue = meta.getMetaValue("quantum.wild.cooldown." + world.getName());
+
+            if (cooldownValue == null) {
+                return 0;
+            }
+
+            return Integer.parseInt(cooldownValue);
+        }
+
+        final @Nullable String cooldownValue = meta.getMetaValue(("quantum.wild.cooldown"));
+
+        if (cooldownValue == null) {
+            return 0;
+        }
+
+        return Integer.parseInt(cooldownValue);
     }
 
     public long getCurrentCooldown(final @NonNull Player player) {
