a14e696e0cae020884d47c7256df4e948e382e70
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-19 12:50:26 -0500

    [QuantumWild] provide essentials api

--- QuantumWild/src/main/java/dev/kscott/quantumwild/module/IntegrationsModule.java
+++ QuantumWild/src/main/java/dev/kscott/quantumwild/module/IntegrationsModule.java
@@ -1,5 +1,6 @@
 package dev.kscott.quantumwild.module;
 
+import com.earth2me.essentials.Essentials;
 import com.google.inject.AbstractModule;
 import com.google.inject.Inject;
 import com.google.inject.Provides;
@@ -19,6 +20,21 @@ public class IntegrationsModule extends AbstractModule {
         final @Nullable RegisteredServiceProvider<LuckPerms> rsp = plugin.getServer().getServicesManager().getRegistration(LuckPerms.class);
 
         if (rsp == null) {
+            plugin.getLogger().warning("Features that depend on the LuckPerms API will not function correctly.");
+            return null;
+        }
+
+        return rsp.getProvider();
+    }
+
+    @Provides
+    @Singleton
+    @Inject
+    public @Nullable Essentials provideEssentialsApi(final @NonNull JavaPlugin plugin) {
+        final @Nullable RegisteredServiceProvider<Essentials> rsp = plugin.getServer().getServicesManager().getRegistration(Essentials.class);
+
+        if (rsp == null) {
+            plugin.getLogger().warning("Features that depend on the EssentialsX API will not function correctly.");
             return null;
         }
 
