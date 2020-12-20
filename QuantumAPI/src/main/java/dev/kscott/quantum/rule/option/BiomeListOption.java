5c523ffdacdb036458af81a1a040afbddf6fa69d
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-20 00:42:21 -0500

    [QuantumAPI] add BiomeListOption

---
+++ QuantumAPI/src/main/java/dev/kscott/quantum/rule/option/BiomeListOption.java
@@ -0,0 +1,10 @@
+package dev.kscott.quantum.rule.option;
+
+public class BiomeListOption extends QuantumRuleOption<String[]> {
+    /**
+     * Constructs the QuantumRuleOption
+     */
+    public BiomeListOption() {
+        super("biome-typess");
+    }
+}
