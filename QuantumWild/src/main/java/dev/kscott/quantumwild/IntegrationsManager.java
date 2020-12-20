2bba4429d7bc85a1873d4eea7d54a8d1cb0f57ec
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-19 21:16:16 -0500

    [QuantumWild] javadoc IntegrationsManager

--- QuantumWild/src/main/java/dev/kscott/quantumwild/IntegrationsManager.java
+++ QuantumWild/src/main/java/dev/kscott/quantumwild/IntegrationsManager.java
@@ -7,14 +7,31 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
+/**
+ * Manages integrations with other plugins.
+ */
 public class IntegrationsManager {
 
+    /**
+     * JavaPlugin reference
+     */
     private final @NonNull JavaPlugin plugin;
 
+    /**
+     * Essentials reference
+     */
     private @Nullable Essentials essentials;
 
+    /**
+     * LuckPerms reference
+     */
     private @Nullable LuckPerms luckPerms;
 
+    /**
+     * Constructs IntegrationsManager
+     *
+     * @param plugin {@link this#plugin}
+     */
     public IntegrationsManager(final @NonNull JavaPlugin plugin) {
         this.plugin = plugin;
 
@@ -34,18 +51,38 @@ public class IntegrationsManager {
         }
     }
 
-    public Essentials getEssentials() {
+    /**
+     * Returns {@link Essentials} if it is enabled
+     *
+     * @return Essentials instance, may be null if {@link this#isEssentialsEnabled()} is false
+     */
+    public @Nullable Essentials getEssentials() {
         return essentials;
     }
 
-    public LuckPerms getLuckPerms() {
+    /**
+     * Returns {@link LuckPerms} if it is enabled
+     *
+     * @return Essentials instance, may be null if {@link this#isLuckPermsEnabled()} is false
+     */
+    public @Nullable LuckPerms getLuckPerms() {
         return luckPerms;
     }
 
+    /**
+     * Checks if EssentialsX is enabled
+     *
+     * @return true if enabled, false if not
+     */
     public boolean isEssentialsEnabled() {
         return plugin.getServer().getPluginManager().getPlugin("Essentials") != null;
     }
 
+    /**
+     * Checks if LuckPerms is enabled
+     *
+     * @return true if enabled, false if not
+     */
     public boolean isLuckPermsEnabled() {
         return plugin.getServer().getPluginManager().getPlugin("LuckPerms") != null;
     }
