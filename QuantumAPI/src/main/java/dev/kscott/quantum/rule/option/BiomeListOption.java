caa48f09dce54b382f0a8b36008444384b433a0f
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-21 20:09:12 -0500

    [QuantumWild] fix BiomeListOption id typo

--- QuantumAPI/src/main/java/dev/kscott/quantum/rule/option/BiomeListOption.java
+++ QuantumAPI/src/main/java/dev/kscott/quantum/rule/option/BiomeListOption.java
@@ -5,6 +5,6 @@ public class BiomeListOption extends QuantumRuleOption<String[]> {
      * Constructs the QuantumRuleOption
      */
     public BiomeListOption() {
-        super("biome-typess");
+        super("biome-types");
     }
 }
