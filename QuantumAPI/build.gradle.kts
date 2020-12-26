fc573ffe3948377e11bc23a94f0d4526de7e1097
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-25 23:06:35 -0500

    [QuantumSpawn/Wild] make stuff compileOnly

--- QuantumAPI/build.gradle.kts
+++ QuantumAPI/build.gradle.kts
@@ -3,11 +3,11 @@ repositories {
 }
 
 dependencies {
-    implementation("org.spongepowered:configurate-hocon:4.0.0")
+    compileOnly("org.spongepowered:configurate-hocon:4.0.0")
 
-    implementation("net.kyori:adventure-api:4.3.0")
-    implementation("net.kyori:adventure-platform-bukkit:4.0.0-SNAPSHOT")
-    implementation("net.kyori:adventure-text-minimessage:4.1.0-SNAPSHOT")
+    compileOnly("net.kyori:adventure-api:4.3.0")
+    compileOnly("net.kyori:adventure-platform-bukkit:4.0.0-SNAPSHOT")
+    compileOnly("net.kyori:adventure-text-minimessage:4.1.0-SNAPSHOT")
 
-    api("cloud.commandframework:cloud-paper:1.3.0-SNAPSHOT")
+    compileOnly("cloud.commandframework:cloud-paper:1.3.0-SNAPSHOT")
 }
