4ec4f4dc87ab1a23a2bc59a65f81efa5589598c5
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-25 22:59:52 -0500

    [QuantumSpawn] add PlayerFirstJoinListener

---
+++ QuantumSpawn/src/main/java/dev/kscott/quantumspawn/listeners/PlayerFirstJoinListener.java
@@ -0,0 +1,89 @@
+package dev.kscott.quantumspawn.listeners;
+
+import com.google.inject.Inject;
+import dev.kscott.quantum.location.LocationProvider;
+import dev.kscott.quantum.location.QuantumLocation;
+import dev.kscott.quantum.rule.ruleset.QuantumRuleset;
+import dev.kscott.quantumspawn.config.Config;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class PlayerFirstJoinListener implements Listener {
+
+    /**
+     * Config reference.
+     */
+    private final @NonNull Config config;
+
+    /**
+     * JavaPlugin reference.
+     */
+    private final @NonNull JavaPlugin plugin;
+
+    /**
+     * LocationProvider reference.
+     */
+    private final @NonNull LocationProvider locationProvider;
+
+    /**
+     * Constructs PlayerJoinListener.
+     *
+     * @param locationProvider LocationProvider reference.
+     */
+    @Inject
+    public PlayerFirstJoinListener(
+            final @NonNull JavaPlugin plugin,
+            final @NonNull Config config,
+            final @NonNull LocationProvider locationProvider
+    ) {
+        this.config = config;
+        this.plugin = plugin;
+        this.locationProvider = locationProvider;
+    }
+
+    @EventHandler
+    public void onPlayerFirstJoin(final @NonNull PlayerJoinEvent event) {
+        final @NonNull Player player = event.getPlayer();
+
+        if (player.hasPlayedBefore()) {
+            return;
+        }
+
+        final @NonNull World world = player.getWorld();
+
+        final @Nullable QuantumRuleset ruleset = config.getRuleset(world);
+
+        if (ruleset == null) {
+            return;
+        }
+
+        final @NonNull CompletableFuture<QuantumLocation> cf = this.locationProvider.getSpawnLocation(ruleset);
+
+        cf.exceptionally(err -> {
+            this.plugin.getLogger().warning("Failed to generate a spawn (spawn-on-join) for "+player.getName()+": "+err.getMessage());
+            return null;
+        });
+
+        if (!cf.isCompletedExceptionally()) {
+            cf.thenAccept(quantumLocation -> {
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        player.teleportAsync(quantumLocation.getLocation());
+                    }
+                }.runTask(plugin);
+            });
+        }
+
+    }
+
+}
