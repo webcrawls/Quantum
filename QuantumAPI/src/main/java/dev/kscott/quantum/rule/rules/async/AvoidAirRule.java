8fc4b7eb690e4b4e64d45ae9dd1992382b206b35
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2021-01-15 21:05:12 -0500

    [QuantumAPI] add AvoidAirRule

---
+++ QuantumAPI/src/main/java/dev/kscott/quantum/rule/rules/async/AvoidAirRule.java
@@ -0,0 +1,24 @@
+package dev.kscott.quantum.rule.rules.async;
+
+import org.bukkit.ChunkSnapshot;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+/**
+ * A rule which blocks locations which are air.
+ */
+public class AvoidAirRule extends AsyncQuantumRule {
+
+    /**
+     * Invalidates locations based on whether or not the block type is air.
+     *
+     * @param snapshot snapshot of the chunk
+     * @param x        x coordinate, relative to chunk (0-15)
+     * @param y        y coordinate (0-255)
+     * @param z        z coordinate, relative to chunk (0-15)
+     * @return true if block is not air, false if it is
+     */
+    @Override
+    public boolean validate(@NonNull ChunkSnapshot snapshot, int x, int y, int z) {
+        return !snapshot.getBlockData(x, y, z).getMaterial().isAir();
+    }
+}
