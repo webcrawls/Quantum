32975bdb31c2932b2f29bbc1adb50c1c754a8333
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2021-01-15 21:37:17 -0500

    [QuantumAPI] turn HighestPossibleYLocator into a RangeYLocator

--- QuantumAPI/src/main/java/dev/kscott/quantum/location/locator/RangeYLocator.java
+++ QuantumAPI/src/main/java/dev/kscott/quantum/location/locator/RangeYLocator.java
@@ -4,25 +4,25 @@ import org.bukkit.ChunkSnapshot;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 /**
- * A YLocator that searches between a min Y and a max Y
+ * A YLocator that searches between a min Y and a max Y.
  */
 public class RangeYLocator implements YLocator {
 
     /**
-     * The minimum Y to start checking from
+     * The minimum Y to start checking from.
      */
     private final int minY;
 
     /**
-     * The maximum Y to stop checking at
+     * The maximum Y to stop checking at.
      */
     private final int maxY;
 
     /**
-     * Constructs RangeYValidator
+     * Constructs RangeYValidator.
      *
-     * @param minY {@link this#minY}
-     * @param maxY {@link this#maxY}
+     * @param minY {@link this#minY}.
+     * @param maxY {@link this#maxY}.
      */
     public RangeYLocator(final int minY, final int maxY) {
         this.minY = minY;
@@ -30,22 +30,38 @@ public class RangeYLocator implements YLocator {
     }
 
     /**
-     * @param snapshot The snapshot of the chunk to look for
-     * @param x        the X coordinate (relative to chunk, 0-15)
-     * @param z        the Z coordinate (relative to chunk, 0-15)
-     * @return the lowest valid Y value between {@link this#minY} and {@link this#maxY}
+     * @param snapshot The snapshot of the chunk to look for.
+     * @param x        the X coordinate (relative to chunk, 0-15).
+     * @param z        the Z coordinate (relative to chunk, 0-15).
+     * @return the lowest valid Y value between {@link this#minY} and {@link this#maxY}.
      */
     @Override
     public int getValidY(@NonNull ChunkSnapshot snapshot, int x, int z) {
-        for (int y = minY; y <= maxY; y++) {
-            final boolean yClear = snapshot.getBlockType(x, y, z).isAir();
-            final boolean yAboveClear = snapshot.getBlockType(x, y + 1, z).isAir();
+        if (maxY > minY) {
+            for (int y = minY; y <= maxY; y++) {
+                final boolean yBelowSafe = snapshot.getBlockType(x, y-1, z).isSolid();
+                final boolean yClear = snapshot.getBlockType(x, y, z).isAir();
+                final boolean yAboveClear = snapshot.getBlockType(x, y + 1, z).isAir();
 
-            if (yClear && yAboveClear) {
-                return y;
+                if (yClear && yAboveClear && yBelowSafe) {
+                    return y;
+                }
+            }
+        } else {
+            for (int y = minY; y >= maxY; y--) {
+                final boolean yBelowSafe = snapshot.getBlockType(x, y-1, z).isSolid();
+                final boolean yClear = snapshot.getBlockType(x, y, z).isAir();
+                final boolean yAboveClear = snapshot.getBlockType(x, y+1, z).isAir();
+
+                if (yBelowSafe && yClear && yAboveClear) {
+                    System.out.println("pog");
+                    return y;
+                }
             }
         }
 
         return -1;
+
+
     }
 }
