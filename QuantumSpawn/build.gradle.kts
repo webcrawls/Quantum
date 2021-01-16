eab8919e58496d0fb3fa7e3bf20ff972e89dd5e4
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2021-01-15 22:32:26 -0500

    [Quantum] Update to cloud 1.4.0.

--- QuantumSpawn/build.gradle.kts
+++ QuantumSpawn/build.gradle.kts
@@ -11,5 +11,5 @@ dependencies {
     compileOnly("net.kyori:adventure-platform-bukkit:4.0.0-SNAPSHOT")
     compileOnly("net.kyori:adventure-text-minimessage:4.1.0-SNAPSHOT")
 
-    compileOnly("cloud.commandframework:cloud-paper:1.3.0-SNAPSHOT")
+    compileOnly("cloud.commandframework:cloud-paper:1.4.0")
 }
--- QuantumWild/build.gradle.kts
+++ QuantumWild/build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     compileOnly("net.kyori:adventure-platform-bukkit:4.0.0-SNAPSHOT")
     compileOnly("net.kyori:adventure-text-minimessage:4.1.0-SNAPSHOT")
 
-    compileOnly("cloud.commandframework:cloud-paper:1.3.0-SNAPSHOT")
+    compileOnly("cloud.commandframework:cloud-paper:1.4.0")
 
     compileOnly("net.luckperms:api:5.2")
 
