971e44b210ba32304ab732a98a954991ccdb538b
kadenscott <kscottdev@gmail.com>
2021-04-28 02:00:39 -0400

    refactor: remove redundant semicolon

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,7 +76,7 @@ subprojects {
             fun relocates(vararg dependencies: String) {
                 dependencies.forEach {
                     val split = it.split('.')
-                    val name = split.last();
+                    val name = split.last()
                     relocate(it, "${rootProject.group}.dependencies.$name")
                 }
             }
