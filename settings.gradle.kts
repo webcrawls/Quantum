0c9baf27f49000c1920900ed4567236af3353360
broccolai <me@broccol.ai>
2021-05-01 01:32:10 +0100

    Use typesafe project accessors

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,3 +4,5 @@ listOf("API", "Spawn", "Wild").forEach {
     include(":Quantum$it")
     project(":Quantum$it").projectDir = file("Quantum$it")
 }
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
