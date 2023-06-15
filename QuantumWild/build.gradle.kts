a43430e0fcf86c1ee7535d62f909e453565f1d10
webcrawls <git@kaden.sh>
2023-06-14 23:37:37 -0400

    👷(gradle) Update Sonatype OSS URL and `net.kyori.adventure` versions

--- QuantumWild/build.gradle.kts
+++ QuantumWild/build.gradle.kts
@@ -13,8 +13,8 @@ dependencies {
     compileOnly("org.spongepowered:configurate-hocon:4.0.0")
 
     compileOnly("net.kyori:adventure-api:4.3.0")
-    compileOnly("net.kyori:adventure-platform-bukkit:4.0.0-SNAPSHOT")
-    compileOnly("net.kyori:adventure-text-minimessage:4.1.0-SNAPSHOT")
+    compileOnly("net.kyori:adventure-platform-bukkit:4.3.0")
+    compileOnly("net.kyori:adventure-text-minimessage:4.14.0")
 
     compileOnly("cloud.commandframework:cloud-paper:1.4.0")
     implementation("cloud.commandframework:cloud-minecraft-extras:1.4.0") {
